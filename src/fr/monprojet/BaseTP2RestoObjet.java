9b5e6c913c5463baceac04d4d5507ecda83ea41e
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-09-22 15:54:36 +0200

    suppression des import inutilisés

--- src/fr/monprojet/BaseTP2RestoObjet.java
+++ src/fr/monprojet/BaseTP2RestoObjet.java
@@ -1,10 +1,5 @@
 package fr.monprojet;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
